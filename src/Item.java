8ca1db14d2a9de950e2cc909c98c672e6b27d4cb
MariiaMira <roze2117@gmail.com>
2024-04-01 11:14:02 +0200

    Interfaces added

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,11 @@
 abstract class Item implements Priceable{
+    private final String name;
 
+    protected Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
