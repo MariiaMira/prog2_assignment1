e5bd96e808298f9785108de1e7bc4af4c7c434f9
MariiaMira <roze2117@gmail.com>
2024-04-01 11:36:54 +0200

    Interfaces added

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,16 @@
-public class Book {
+public class Book extends Item implements PriceableWithVAT6{
+    private final Double price;
+    private final boolean bound;
+    private final String author;
 
+    public Book (String name, String author, Double price, boolean bound){
+        super(name);
+        this.author = author;
+        this.price = price;
+        this.bound = bound;
+    }
+
+    public double getPrice(){
+        return bound ? price * 1.3 : price;
+    }
 }
