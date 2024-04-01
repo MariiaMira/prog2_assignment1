4a539861e3d69e1c60c04f063e358537cc775289
Ronja01010101 <ronja01010101@gmail.com>
2024-04-01 11:20:02 +0200

    askjdalksdjak

---
+++ src/Recording.java
@@ -0,0 +1,22 @@
+public abstract class Recording extends Item{
+    private String name;
+    private String artist;
+    private int year;
+    private int condition;
+    private double price;
+
+    protected Recording(String name, String artist, int year, int condition, double price){
+        this.name = name;
+        this.artist = artist;
+        this.year = year;
+
+
+        this.condition = condition; // värde mellan 0-10
+        this.price = price;
+    }
+
+
+
+
+
+}
